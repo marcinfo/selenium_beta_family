4bc1de1535dbf78fbf497ef7f8217f72b71081ca
marcinfo <marc_info@yahoo.com.br>
2025-04-12 14:00:26 -0300

    contatos ok

---
+++ Selecionando_primeira_linha_span.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.common.by import By
+import time
+
+navegador = webdriver.Chrome()
+
+navegador.get("https://beta.familyofficelist.org/sign-in")
+navegador.maximize_window()
+time.sleep(2)
+navegador.find_element("id", ':r0:').send_keys('admin@strategic-cap.com')
+navegador.find_element("id", ':r1:').send_keys('StrategicCapital2025#')
+navegador.find_element("xpath", '//*[@id="root"]/div[1]/div/div[1]/div/form/div[5]/button').click()
+time.sleep(2)
+navegador.get("https://beta.familyofficelist.org/my-data")
+time.sleep(5)
+
+tabela_movimentacoes = navegador.find_element("xpath", '//*[@id="table"]/tbody')
+
+my_data = []
+
+while True:
+    for tr in tabela_movimentacoes.find_elements("tag name", "tr"):
+        navegador.find_element("xpath", '//*[@id="table"]/tbody/tr[1]/td[2]/div/div/p').click()
+        time.sleep(1)
+        spans = navegador.find_elements(By.CSS_SELECTOR,
+                                        'div.MuiPopover-root.MuiModal-root.css-jp7szo > div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation8.MuiPopover-paper.css-kteami-popper-popper')
+        time.sleep(1)
+
+        navegador.find_element(By.CSS_SELECTOR, 'body > div.MuiPopover-root.MuiModal-root.css-jp7szo > div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation8.MuiPopover-paper.css-kteami-popper-popper > div > div:nth-child(3) > div:nth-child(2) > div > button:nth-child(2)').click()
+        for span in spans:
+            a_tags = span.find_elements(By.TAG_NAME, 'a')
+            p_tags = span.find_elements(By.TAG_NAME, 'p')
+            h3_tags = span.find_elements(By.TAG_NAME, 'h3')
+            h2_tags = span.find_elements(By.TAG_NAME, 'h2')
+            spaner = span.find_elements(By.TAG_NAME, 'span')
+            for tag in h3_tags + spaner + a_tags + p_tags + h2_tags :
+                my_data.append(tag.text)
+
+        navegador.find_element(By.XPATH, "//span[@aria-label='Close']/button").click()
+        print(my_data)
+        time.sleep(50)
+navegador.quit()
