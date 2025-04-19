4daaa0b8c12e84f972be40b974900f8df72d3474
marcinfo <marc_info@yahoo.com.br>
2025-04-19 14:04:43 -0300

    dados scraping ok falta exportar para csv e acertar retorno

--- 18_04_25.py
+++ 18_04_25.py
@@ -5,15 +5,21 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-extensions")
 
-navegador = webdriver.Chrome()
+
+navegador = webdriver.Chrome(options=options)
+
 navegador.get("https://beta.familyofficelist.org/sign-in")
 navegador.maximize_window()
 time.sleep(2)
 navegador.find_element("id", ':r0:').send_keys('admin@strategic-cap.com')
 navegador.find_element("id", ':r1:').send_keys('StrategicCapital2025#')
 navegador.find_element("xpath", '//*[@id="root"]/div[1]/div/div[1]/div/form/div[5]/button').click()
-time.sleep(2)
+time.sleep(4)
 navegador.get("https://beta.familyofficelist.org/my-data")
 
 WebDriverWait(navegador, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="table"]/tbody')))
@@ -25,7 +31,18 @@ i = 0
 while True:
     for tr in tabela_movimentacoes.find_elements(By.TAG_NAME, "tr"):
         for td in tr.find_elements(By.TAG_NAME, "td"):
-            pass
+            td = tr.find_element(By.XPATH, 'td[2]')
+             # Localiza o elemento da empresa dentro da célula
+            company = td.find_element(By.XPATH, 'div/div/p')
+            site = td.find_element(By.XPATH, '//*[@id="table"]/tbody/tr[1]/td[3]/span/p/a')
+            office_type= td.find_element(By.XPATH, '//*[@id="table"]/tbody/tr[1]/td[4]/p')
+            country = td.find_element(By.XPATH, '//*[@id="table"]/tbody/tr[1]/td[5]/div/p')
+            city = td.find_element(By.XPATH, '//*[@id="table"]/tbody/tr[1]/td[6]/p')
+            company = company.text
+            site=site.text
+            office = office_type.text
+            country = country.text
+            city = city.text
         try:
             WebDriverWait(navegador, 10).until(
                 EC.element_to_be_clickable((By.XPATH, f'//*[@id="table"]/tbody/tr[{i + 1}]/td[2]/div/div/p'))).click()
@@ -59,9 +76,10 @@ while True:
                                enumerate(zip(nomes_texto, cargos_texto, phones_texto), start=1)]"""
 
             for nome, cargo,phone,email in zip(nomes_texto, cargos_texto, phones_texto,emails_texto):
-                my_data.append({"id":i+1,"nome": nome, "cargo": cargo, "phone": phone,"e-mail":email})
+                my_data.append({"id":i+1,"company":company,"web site":site,"office Type":office,"country":country,"city":city,\
+                                "contact": nome, "position": cargo, "phone": phone,"e-mail":email})
                 print(my_data)
-            ##
+
 
             """for dado in dados:
                 print(f"Nome: {dado['nome']}, Cargo: {dado['cargo']}")"""
